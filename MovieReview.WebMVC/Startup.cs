697c6da9bfca6fa31499bfdcd83893e4ca814802
akembrar7795 <akembrar7795@gmail.com>
2018-08-16 10:29:14 -0400

    Add project files.

---
+++ MovieReview.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieReview.WebMVC.Startup))]
+namespace MovieReview.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
